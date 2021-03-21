ce0645e10aeee72804d29963f8215afd5d468edd
JithuAntony <JithuAntony@LAPTOP-JITHU>
2021-03-21 18:18:42 +0530

    Added the sample home and register component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RegisterFormComponent } from './register-form/register-form.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { ViewStudentDetailsComponent } from './view-student-details/view-student-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegisterFormComponent,
+    HomePageComponent,
+    ViewStudentDetailsComponent,
   ],
   imports: [
     BrowserModule,
