53e3f8acd09eb23761a4be92a907dbfbdc8d243d
JithuAntony <JithuAntony@LAPTOP-JITHU>
2021-03-21 18:34:18 +0530

    Added routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomePageComponent } from './home-page/home-page.component';
+import { RegisterFormComponent } from './register-form/register-form.component';
+import { ViewStudentDetailsComponent } from './view-student-details/view-student-details.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomePageComponent },
+  { path: 'register', component: RegisterFormComponent },
+  { path: 'view-student', component: ViewStudentDetailsComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/view-student-details/view-student-details.component.ts
+++ src/app/view-student-details/view-student-details.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-view-student-details',
   templateUrl: './view-student-details.component.html',
-  styleUrls: ['./view-student-details.component.css']
+  styleUrls: ['./view-student-details.component.scss']
 })
 export class ViewStudentDetailsComponent implements OnInit {
 
